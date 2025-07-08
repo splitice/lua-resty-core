b7abd36cfe87848d763037861975c3a6a41c9580
Mathew <mat999@gmail.com>
2025-07-08 15:46:40 +1000

    add ngx.skip_body_filter

--- lib/resty/core/exit.lua
+++ lib/resty/core/exit.lua
@@ -16,7 +16,7 @@ local co_yield = coroutine._yield
 local subsystem = ngx.config.subsystem
 
 
-local ngx_lua_ffi_exit, ngx_http_lua_ngx_staticfile_ffi
+local ngx_lua_ffi_exit, ngx_http_lua_ngx_staticfile_ffi, ngx_http_lua_ngx_skipbodyfilter_ffi
 
 
 if subsystem == "http" then
@@ -25,10 +25,12 @@ if subsystem == "http" then
                                unsigned char *err, size_t *errlen);
     
     int ngx_http_lua_ngx_staticfile_ffi(ngx_http_request_t *r, const char *p, size_t len);
+    void ngx_http_lua_ngx_skipbodyfilter_ffi(ngx_http_request_t *r);
 ]]
 
     ngx_lua_ffi_exit = C.ngx_http_lua_ffi_exit
     ngx_http_lua_ngx_staticfile_ffi = C.ngx_http_lua_ngx_staticfile_ffi
+    ngx_http_lua_ngx_skipbodyfilter_ffi = C.ngx_http_lua_ngx_skipbodyfilter_ffi
 
 elseif subsystem == "stream" then
     ffi.cdef[[
@@ -71,6 +73,15 @@ ngx.staticfile = function(path)
     end
 
     ngx_http_lua_ngx_staticfile_ffi(r, path, #path)
+end
+
+ngx.skip_body_filter = function()
+    local r = get_request()
+    if not r then
+        error("no request found")
+    end
+
+    ngx_http_lua_ngx_skipbodyfilter_ffi(r)
 end
 
 return {
